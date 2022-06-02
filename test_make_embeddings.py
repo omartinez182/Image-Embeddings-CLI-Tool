49a9cfac17a0b99aea059ef6a667a94b8a18a245
omartinez@domalabs.com <omartinez@domalabs.com>
2022-06-01 20:45:13 -0400

    Updates to embedding tests

--- test_make_embeddings.py
+++ test_make_embeddings.py
@@ -1,6 +1,16 @@
 from make_embeddings import main
+import argparse
 
 def test_embeddings_size():
-    allVectors = main()
-    for vec in allVectors:
-        assert len(allVectors[vec]) == 512
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--URL',
+                        type=str)
+        parser.add_argument('--inputDir',
+                            type=str,
+                            default='data/images')
+        arguments = parser.parse_args()
+        allVectors = main(arguments)
+
+        for vec in allVectors:
+            assert len(allVectors[vec]) == 512
